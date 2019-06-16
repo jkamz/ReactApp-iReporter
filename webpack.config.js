c65267fdea038c9e09d67c4b16522bbf3370cac2
jkamz <jkamz6432@gmail.com>
2019-06-16 21:51:55 +0300

    add express server config for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+
+    devtool: 'source-map',
+
     entry: './src/index.js',
     output: {
         path: path.join(__dirname, '/dist'),
